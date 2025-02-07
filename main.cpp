a2d5e2286380c3bf9f4b4f582e957cf6b0a25de9
eso8086 <elicodingyt@gmail.com>
2025-02-08 03:00:30 +0400

    refactoring done

--- main.cpp
+++ main.cpp
@@ -14,21 +14,20 @@
 #define WINDOW_H 600
 
 
-struct AppContext{
+
+struct {
     SDL_Window* window;
-    SDL_GLContext main_context;
-};
+    SDL_GLContext gl_context;
+} app;
 
 //startup
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char* argv[]){
     SDL_SetAppMetadata("My OpenGL App", "0.1", "com.example.opengl");
 
-
     if(!SDL_Init(SDL_INIT_VIDEO)){
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
-
 
     SDL_GL_LoadLibrary(NULL);
     SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
@@ -37,17 +36,19 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char* argv[]){
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
-    SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, WINDOW_W, WINDOW_H, SDL_WINDOW_OPENGL);
-    if(!window){
+    app.window = SDL_CreateWindow(WINDOW_TITLE, WINDOW_W, WINDOW_H, SDL_WINDOW_OPENGL);
+    if(!app.window){
         SDL_Log("Couldn't create window: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    SDL_GLContext maincontext = SDL_GL_CreateContext(window);
-    if(!maincontext){
+
+    app.gl_context = SDL_GL_CreateContext(app.window);
+    if(!app.gl_context){
         SDL_Log("Couldn't create OpenGL context: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+
     gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress);
 
     SDL_Log("Vendor: %s", glGetString(GL_VENDOR));
@@ -57,14 +58,6 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char* argv[]){
     SDL_GL_SetSwapInterval(1);
     glDisable(GL_DEPTH_TEST);
     glDisable(GL_CULL_FACE);
-
-
-    *appstate = new AppContext{
-        window,
-        maincontext,
-    };
-
-
 
     SDL_Log("Application started successfully");
 
@@ -79,36 +72,29 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event *event){
     return SDL_APP_CONTINUE;
 }
 
-//frame
+//RENDER
 SDL_AppResult SDL_AppIterate(void* appstate){
-    auto* app = (AppContext*) appstate;
-
     const double now = ((double)SDL_GetTicks()) / 1000.0;
 
     glViewport(0, 0, WINDOW_W, WINDOW_H);
 
-
-    //RENDER
-
-
-
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-
-    SDL_GL_SwapWindow(app->window);
+    SDL_GL_SwapWindow(app.window);
 
     return SDL_APP_CONTINUE;
 }
 
 //clean up
 void SDL_AppQuit(void* appstate, SDL_AppResult result){
-    //clean up if needed
-    auto* app = (AppContext*)appstate;
-    if(app){
-        SDL_DestroyWindow(app->window);
-        delete app;
+    if(result != SDL_APP_SUCCESS){
+        SDL_Log("Application quit successfully");
     }
+    else{
+        SDL_Log("Application quit unsuccessfully");
+    }
+
+    SDL_DestroyWindow(app.window);
     SDL_Quit();
-    SDL_Log("Application quit successfully");
 }
